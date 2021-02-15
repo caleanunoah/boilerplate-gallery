846d9b3cc60b4bde87472fc012e0250406adbe1b
caleanunoah <noahcaleanu152@gmail.com>
2021-02-14 16:41:08 -0800

    fix

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -164,6 +164,7 @@ class HomePage extends Component {
             <Footer></Footer>
             <Footer></Footer>
             <Footer></Footer>
+            <Footer></Footer>
           </body>
             </>
         );
